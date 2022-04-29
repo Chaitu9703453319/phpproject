b07f5d8aa22f5fc66a7aa27467020bf48c420bcf
root <root@ip-172-31-28-64.ec2.internal>
2022-04-29 13:58:38 +0000

    rst time commit

---
+++ mulpage.java
@@ -0,0 +1,11 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+
+      int num1 = 10, num2 = 5, sum;
+      total = num1 * num2;
+
+      System.out.println("total of these numbers: "+sum);
+   }
+}
+
